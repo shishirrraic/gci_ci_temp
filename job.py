06f8055654c1669f435b888dbd82fc89527496ac
shishirrraic <srai@kent.edu>
2019-12-05 13:39:19 -0500

    committed

--- job.py
+++ job.py
@@ -6,5 +6,5 @@ now = datetime.now()
 print("now =", now)
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print("date and time is =", dt_string)
+print("date and time isasdfads =", dt_string)
 print("date and time is it is it asdf asdf asdfasasdfasdfdf asdaf asdfasdfasdf asdasdfasdff=", dt_string)
